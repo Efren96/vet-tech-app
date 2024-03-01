39eb3a3071e45bfee729cd3146dc793eee63affa
Efren Leal <efren@Efrens-MacBook-Pro-2.local>
2024-02-29 19:08:12 -0600

    created models folder containing the user, pet, owner, and index js files

---
+++ models/pet-profile.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Pet extends Model { }
+
+Pet.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        ownerName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        age: {
+            type: 
+        }
+    }
+)
