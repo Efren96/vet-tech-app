e82fe1851d1ea449c7e74fe8c7eaa5939e54aff5
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-03-07 19:32:16 -0600

    working on login styling

--- controllers/api/petrecord-route.js
+++ controllers/api/petrecord-route.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { petProfile } = require('../../models');
+const { Pet } = require('../../models');
 const sequelize = require('../../config/connection');
 // const withAuth = require('../../utils/auth');
 
