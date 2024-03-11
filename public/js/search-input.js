1257c20f57aa43f0d89696f004499ce9fc42b820
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-03-11 10:38:09 -0500

    commented out future dev logic for search bar

--- public/js/search-input.js
+++ public/js/search-input.js
@@ -1,22 +1,22 @@
-const searchInput = document.querySelector(".search-input");
-const searchBtn = document.querySelector(".search-button")
-console.log(searchInput)
+// const searchInput = document.querySelector(".search-input");
+// const searchBtn = document.querySelector(".search-button")
+// console.log(searchInput)
 
-const searchName = async (event) => {
-    event.stopPropagation();
-    event.preventDefault();
+// const searchName = async (event) => {
+//     event.stopPropagation();
+//     event.preventDefault();
 
-    const response = await fetch(`/api/petdashboard/${searchInput}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-    if (response.ok) {
-        document.location.reload();
-    } else {
-        alert('Failed to find pet');
-    }
-};
+//     const response = await fetch(`/api/petdashboard/${searchInput}`, {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//     });
+//     if (response.ok) {
+//         document.location.reload();
+//     } else {
+//         alert('Failed to find pet');
+//     }
+// };
 
-searchBtn.addEventListener('click', searchName)
+// searchBtn.addEventListener('click', searchName)
