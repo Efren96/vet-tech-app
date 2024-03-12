6bdad41f17e397525278d10718768c1b3721bb43
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-03-11 19:01:44 -0500

    fixed logout button

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,5 +1,7 @@
 // logic for logout button
-const logout = async () => {
+const logout = async (event) => {
+    event.preventDefault();
+    
     const response = await fetch('/api/user/logout', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
